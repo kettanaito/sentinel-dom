082f6a91caab237785059f4cebca652e2d16d42c
Artem Zakharchenko <kettanaito@gmail.com>
2017-09-06 16:05:03 +0200

    Provide JSDoc explanations to helper methods

--- src/utils/ensureArray.js
+++ src/utils/ensureArray.js
@@ -2,7 +2,7 @@
 
 /**
  * Ensure Array.
- * @summary Ensure given variable is encapsulated in the Array (becomes iterable).
+ * @summary Ensure given variable is encapsulated in the Array (and, therefore, iterable).
  */
 export default function ensureArray(variable: Array<any> | any): Array<any> {
   return Array.isArray(variable) ? variable : Array.from(variable);
--- src/utils/getClientRect.js
+++ src/utils/getClientRect.js
@@ -4,6 +4,12 @@
  */
 import type { TClientRect } from '../../types/ClientRect';
 
+/**
+ * @summary Ensures client rectangle has all required dimensions.
+ * Sometimes the end ClientRect received by {HTMLElement.getBoundingClientRect} does not
+ * contain "right" or "bottom". This method ensures the dimensions are present, since those
+ * dimensions may be calculated from the data which is always present.
+ */
 export function getClientRect(element: HTMLElement): TClientRect {
   const rect: ClientRect = element.getBoundingClientRect();
 
